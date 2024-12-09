23d3bd34355fd82740791c851c7ac2b3a168f165
hau <dauphaihau@gmail.com>
2024-12-09 22:42:28 +0700

    fix error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,37 +1,37 @@
 // prisma/seed.ts
 
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
 // initialize Prisma Client
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Han',
-      email: 'han@prisma.io',
-      posts: {
-        create: [
-          {
-            title: 'My first day at Prisma',
-          },
-          {
-            title: 'How to connect to a SQLite database',
-          },
-        ],
-      },
-    },
-  });
-  return user;
-}
+// async function main() {
+//   const user = await prisma.user.create({
+//     data: {
+//       name: 'Han',
+//       email: 'han@prisma.io',
+//       posts: {
+//         create: [
+//           {
+//             title: 'My first day at Prisma',
+//           },
+//           {
+//             title: 'How to connect to a SQLite database',
+//           },
+//         ],
+//       },
+//     },
+//   });
+//   return user;
+// }
 
 // execute the main function
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    // close Prisma Client at the end
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     // close Prisma Client at the end
+//     await prisma.$disconnect();
+//   });
